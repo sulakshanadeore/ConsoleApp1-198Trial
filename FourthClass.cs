67e8614c2f10c06e252d1670f82171fc27698349
deoresulakshana@gmail.com <36725147+sulakshanadeore@users.noreply.github.com>
2021-09-29 09:28:59 +0530

    try static methods

--- FourthClass.cs
+++ FourthClass.cs
@@ -8,5 +8,16 @@ namespace ConsoleApp1
 {
     class FourthClass
     {
+
+        static void Main(string[] args)
+        {
+            FourthClass.M1();
+            Console.Read();
+        }
+
+       internal static void M1()
+        {
+            Console.WriteLine("Static method can be called without creating object of the class \n It is accessed using Classname.MethodName");
+        }
     }
 }
